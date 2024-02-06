06fd0480430144863c475e2c438f5b34ebaac99b
Pratik Kadam <123864464+PratikKadam2244@users.noreply.github.com>
2024-02-06 21:32:11 +0530

    Bank ATM Virtual Guide

---
+++ Bank-Management-System--master/src/AP_BANK_ATM/main_Class.java
@@ -0,0 +1,123 @@
+package AP_BANK_ATM;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class main_Class extends JFrame implements ActionListener {
+    JButton b1,b2,b3,b4,b5,b6,b7;
+    String pin;
+    main_Class(String pin){
+        this.pin = pin;
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/atm2.png"));
+        Image i2 = i1.getImage().getScaledInstance(1550,830,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel l3 = new JLabel(i3);
+        l3.setBounds(0,0,1550,830);
+        add(l3);
+
+        JLabel label1 = new JLabel("AP  BANK  ATM");
+        label1.setBounds(490,15,700,45);
+        label1.setForeground(Color.red);
+        label1.setFont(new Font("System",Font.BOLD,38));
+        l3.add(label1);
+        
+        JLabel label2 = new JLabel("(Virtual Guide)");
+        label2.setBounds(530,60,500,25);
+        label2.setForeground(Color.white);
+        label2.setFont(new Font("System",Font.BOLD,28));
+        l3.add(label2);
+        
+        JLabel label = new JLabel("Please Select Your Transaction");
+        label.setBounds(430,180,700,35);
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("System",Font.BOLD,28));
+        l3.add(label);
+
+        b1 = new JButton("DEPOSIT");
+        b1.setForeground(Color.WHITE);
+        b1.setBackground(new Color(65,125,128));
+        b1.setBounds(410,274,150,35);
+        b1.addActionListener(this);
+        l3.add(b1);
+
+        b2 = new JButton("CASH WITHDRAWL");
+        b2.setForeground(Color.white);
+        b2.setBackground(new Color(65,125,128));
+        b2.setBounds(700,274,150,35);
+        b2.addActionListener(this);
+        l3.add(b2);
+
+        b3 = new JButton("FAST CASH");
+        b3.setForeground(Color.WHITE);
+        b3.setBackground(new Color(65,125,128));
+        b3.setBounds(410,318,150,35);
+        b3.addActionListener(this);
+        l3.add(b3);
+
+        b4 = new JButton("MINI STATEMENT");
+        b4.setForeground(Color.WHITE);
+        b4.setBackground(new Color(65,125,128));
+        b4.setBounds(700,318,150,35);
+        b4.addActionListener(this);
+        l3.add(b4);
+
+        b5 = new JButton("PIN CHANGE");
+        b5.setForeground(Color.white);
+        b5.setBackground(new Color(65,125,128));
+        b5.setBounds(410,362,150,35);
+        b5.addActionListener(this);
+        l3.add(b5);
+
+        b6 = new JButton("BALANCE ENQUIRY");
+        b6.setForeground(Color.WHITE);
+        b6.setBackground(new Color(65,125,128));
+        b6.setBounds(700,362,150,35);
+        b6.addActionListener(this);
+        l3.add(b6);
+
+        b7 = new JButton("EXIT");
+        b7.setForeground(Color.red);
+        b7.setBackground(new Color(65,125,128));
+        b7.setBounds(700,406,150,35);
+        b7.setFont(new Font("System",Font.BOLD,12));
+        b7.addActionListener(this);
+        l3.add(b7);
+
+        setLayout(null);
+        setSize(1550,1080);
+        setLocation(0,0);
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==b1){
+           new Deposit(pin);
+            setVisible(false);
+        }else if (e.getSource()==b7){
+            System.exit(0);
+        } else if (e.getSource()==b2) {
+            new Withdrawl(pin);
+            setVisible(false);
+        } else if (e.getSource()==b6) {
+            new BalanceEnquriy(pin);
+            setVisible(false);
+        } else if (e.getSource()==b3) {
+            new FastCash(pin);
+            setVisible(false);
+        } else if (e.getSource()==b5) {
+            new Pin(pin);
+            setVisible(false);
+        } else if (e.getSource()==b4) {
+            new mini(pin);
+        }
+    }
+
+    public static void main(String[] args) {
+        new main_Class("");
+    }
+}
